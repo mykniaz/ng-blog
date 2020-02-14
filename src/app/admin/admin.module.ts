6be8a288b7b0dd5bea367b82157fe5268c1efccc
mykniaz <mykniaz@gmail.com>
2020-02-14 12:51:53 +0200

    [fe] add base structure for post

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { AdminLayoutComponent } from './shared/compnents/admin-layout/admin-layout.component';
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
-import {MainLayoutComponent} from './shared/compnents/main-layout/main-layout.component';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
 
